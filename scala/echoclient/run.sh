d1fc0a7789664ed782829cedeebf7a00508bc16c
Alexis Agahi <alag@users.sf.net>
2010-07-02 09:32:53 +0200

    Autoreconnect

--- scala/echoclient/run.sh
+++ scala/echoclient/run.sh
@@ -1,3 +1,3 @@
 #JAVA_HOME=
 export MAVEN2_REPO=$HOME/.m2/repository
-$JAVA_HOME/bin/java -Dfile.encoding=UTF-8 -server -jar $MAVEN2_REPO/org/tigris/pomstrap/pomstrap/1.0.14/pomstrap-1.0.14.jar com.treegger.app:echo-client:0.1 com.treegger.app.echo.EchoClient:launch
+exec $JAVA_HOME/bin/java -Dfile.encoding=UTF-8 -server -jar $MAVEN2_REPO/org/tigris/pomstrap/pomstrap/1.0.14/pomstrap-1.0.14.jar com.treegger.app:echo-client:0.1 com.treegger.app.echo.EchoClient:launch
